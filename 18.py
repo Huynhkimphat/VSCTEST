c485b872a2f3f84fd6f52907692c241cc3a3f40b
Huynhkimphat <hkimphat2012@gmail.com>
2020-04-30 17:27:44 +0700

    ADD FULL

--- 18.py
+++ 18.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 page = requests.get(
@@ -7,7 +8,32 @@ week = soup.find(id='seven-day-forecast-body')
 # print(week)
 items = week.find_all(class_='tombstone-container')
 # print(items[0])
-for item in items:
-    print(item.find(class_='period-name').get_text())
-    print(item.find(class_='short-desc').get_text())
-    print(item.find(class_='temp').get_text())
+
+
+# for item in items:
+#     print(item.find(class_='period-name').get_text())
+#     print(item.find(class_='short-desc').get_text())
+#     print(item.find(class_='temp').get_text())
+
+
+period_names = [item.find(class_='period-name').get_text() for item in items]
+short_description = [item.find(class_='short-desc').get_text()
+                     for item in items]
+
+
+temp = [item.find(class_='temp').get_text() for item in items]
+# print(period_names)
+# print(short_description)
+# print(temp)
+
+
+weather_stuff = pd.DataFrame(
+    {
+        'period': period_names,
+        'short_description': short_description,
+        'temperature': temp,
+    }
+)
+print(weather_stuff)
+
+weather_stuff.to_csv('weather.csv')
